ec232d3b17865689c7f221c07718c6bc1c8ee08c
kamal <kamade123@gmail.com>
2026-06-14 10:02:07 +0100

    added Showcase to main app

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Nav from './components/Nav/Nav.jsx'
 import Hero from './components/Hero/Hero.jsx'
 import Manifesto from './components/Manifesto/Manifesto.jsx'
 import PriorityAccess from './components/PriorityAccess/PriorityAccess.jsx'
+import Showcase from './components/Showcase/Showcase.jsx'
 
 export default function App() {
   return (
     <>
       <Nav />
       <Hero />
+      <Showcase />
       <Manifesto />
       <PriorityAccess />
     </>
