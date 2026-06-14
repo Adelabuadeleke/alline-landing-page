b02309b7820a9a651129770b6580a0f1354b1cd0
kamal <kamade123@gmail.com>
2026-06-14 08:21:46 +0100

    added priority access to main App.jsx file...

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Nav from './components/Nav/Nav.jsx'
 import Hero from './components/Hero/Hero.jsx'
 import Manifesto from './components/Manifesto/Manifesto.jsx'
+import PriorityAccess from './components/PriorityAccess/PriorityAccess.jsx'
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Nav />
       <Hero />
       <Manifesto />
+      <PriorityAccess />
     </>
   )
 }
