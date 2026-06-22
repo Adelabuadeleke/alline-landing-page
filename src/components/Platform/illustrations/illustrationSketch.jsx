1c7294572a0a9491500256e909d5abd99944cbf2
kamal <kamade123@gmail.com>
2026-06-22 11:03:27 +0100

    updated illustrtion...

--- src/components/Platform/illustrations/illustrationSketch.jsx
+++ src/components/Platform/illustrations/illustrationSketch.jsx
@@ -65,33 +65,49 @@ const IllustrationSketch = forwardRef(function IllustrationSketch(props, ref) {
     // </svg>
 
     <svg width="302" height="277" viewBox="0 0 302 277" fill="none" xmlns="http://www.w3.org/2000/svg">
-     <path d="M289.924 129.705L289.672 129.849L240.641 157.915L240.393 158.057L240.145 157.916L190.772 129.85L190.519 129.706V44.2441L190.769 44.0996L190.81 44.0762L240.143 15.6436L240.393 15.5L240.643 15.6445L289.675 44.1006L289.924 44.2451V129.705Z" fill="" stroke="#136A0D"/>
-     <path d="M240.134 70.5303V171.506L181.596 138.172V36.7324L240.134 70.5303ZM299.259 138.173L241.134 171.503V70.5283L299.259 36.792V138.173ZM298.764 35.9238L240.633 69.6641L182.146 35.8955L240.632 2.12695L298.764 35.9238Z" stroke="#82FF7A"/>
-     <circle cx="240.635" cy="69.8277" r="1.65385" fill="#82FF7A"/>
-     <circle cx="240.635" cy="171.539" r="1.65385" fill="#82FF7A"/>
-     <rect x="238.442" width="4" height="4" fill="#82FF7A"/>
-     <rect x="179.442" y="34" width="4" height="4" fill="#82FF7A"/>
-     <rect x="179.442" y="136" width="4" height="4" fill="#82FF7A"/>
-     <rect x="296.442" y="34" width="4" height="4" fill="#82FF7A"/>
-     <rect x="297.692" y="136" width="4" height="4" fill="#82FF7A"/>
-     <path d="M201.443 180.147L201.191 180.291L152.161 208.357L151.913 208.499L151.665 208.358L102.292 180.292L102.039 180.148V94.6865L102.289 94.542L102.33 94.5186L151.663 66.0859L151.913 65.9424L152.163 66.0869L201.194 94.543L201.443 94.6875V180.147Z" fill="" stroke="#136A0D"/>
-     <path d="M151.654 120.973V221.948L93.1155 188.614V87.1748L151.654 120.973ZM210.779 188.615L152.654 221.945V120.971L210.779 87.2344V188.615ZM210.283 86.3662L152.153 120.106L93.6653 86.3379L152.152 52.5693L210.283 86.3662Z" stroke="#82FF7A"/>
-     <circle cx="152.154" cy="120.27" r="1.65385" fill="#82FF7A"/>
-     <circle cx="152.154" cy="221.981" r="1.65385" fill="#82FF7A"/>
-     <rect x="149.962" y="50.4424" width="4" height="4" fill="#82FF7A"/>
-     <rect x="90.9617" y="84.4424" width="4" height="4" fill="#82FF7A"/>
-     <rect x="90.9617" y="186.442" width="4" height="4" fill="#82FF7A"/>
-     <rect x="207.962" y="84.4424" width="4" height="4" fill="#82FF7A"/>
-     <rect x="209.212" y="186.442" width="4" height="4" fill="#82FF7A"/>
-     <path d="M110.481 232.705L110.229 232.849L61.1991 260.915L60.951 261.057L60.703 260.916L11.33 232.85L11.077 232.706V147.244L11.327 147.1L11.368 147.076L60.701 118.644L60.951 118.5L61.201 118.645L110.232 147.101L110.481 147.245V232.705Z" fill="" stroke="#136A0D"/>
-     <path d="M60.6919 173.53V274.506L2.15381 241.172V139.732L60.6919 173.53ZM119.817 241.173L61.6919 274.503V173.528L119.817 139.792V241.173ZM119.322 138.924L61.1909 172.664L2.70361 138.896L61.1899 105.127L119.322 138.924Z" stroke="#82FF7A"/>
-     <circle cx="61.1923" cy="172.828" r="1.65385" fill="#82FF7A"/>
-     <circle cx="61.1923" cy="274.539" r="1.65385" fill="#82FF7A"/>
-     <rect x="59" y="103" width="4" height="4" fill="#82FF7A"/>
-     <rect y="137" width="4" height="4" fill="#82FF7A"/>
-     <rect y="239" width="4" height="4" fill="#82FF7A"/>
-     <rect x="117" y="137" width="4" height="4" fill="#82FF7A"/>
-     <rect x="118.25" y="239" width="4" height="4" fill="#82FF7A"/>
+     <g data-draw-group>
+        <g data-cube-group="0">
+         <path data-stroke-path d="M110.481 232.705L110.229 232.849L61.1991 260.915L60.951 261.057L60.703 260.916L11.33 232.85L11.077 232.706V147.244L11.327 147.1L11.368 147.076L60.701 118.644L60.951 118.5L61.201 118.645L110.232 147.101L110.481 147.245V232.705Z" fill="" stroke="#136A0D"/>
+         <path data-stroke-path d="M60.6919 173.53V274.506L2.15381 241.172V139.732L60.6919 173.53ZM119.817 241.173L61.6919 274.503V173.528L119.817 139.792V241.173ZM119.322 138.924L61.1909 172.664L2.70361 138.896L61.1899 105.127L119.322 138.924Z" stroke="#82FF7A"/>
+         <circle data-accent cx="61.1923" cy="172.828" r="1.65385" fill="#82FF7A"/>
+         <circle data-accent cx="61.1923" cy="274.539" r="1.65385" fill="#82FF7A"/>
+         <rect data-accent x="59" y="103" width="4" height="4" fill="#82FF7A"/>
+         <rect data-accent y="137" width="4" height="4" fill="#82FF7A"/>
+         <rect data-accent y="239" width="4" height="4" fill="#82FF7A"/>
+         <rect data-accent x="117" y="137" width="4" height="4" fill="#82FF7A"/>
+         <rect data-accent x="118.25" y="239" width="4" height="4" fill="#82FF7A"/>
+        </g>
+     </g>
+
+     
+
+     <g data-draw-group>
+        <g data-cube-group="1">
+          <path data-stroke-path d="M201.443 180.147L201.191 180.291L152.161 208.357L151.913 208.499L151.665 208.358L102.292 180.292L102.039 180.148V94.6865L102.289 94.542L102.33 94.5186L151.663 66.0859L151.913 65.9424L152.163 66.0869L201.194 94.543L201.443 94.6875V180.147Z" fill="" stroke="#136A0D"/>
+          <path data-stroke-path d="M151.654 120.973V221.948L93.1155 188.614V87.1748L151.654 120.973ZM210.779 188.615L152.654 221.945V120.971L210.779 87.2344V188.615ZM210.283 86.3662L152.153 120.106L93.6653 86.3379L152.152 52.5693L210.283 86.3662Z" stroke="#82FF7A"/>
+          <circle data-accent cx="152.154" cy="120.27" r="1.65385" fill="#82FF7A"/>
+          <circle data-accent cx="152.154" cy="221.981" r="1.65385" fill="#82FF7A"/>
+          <rect data-accent x="149.962" y="50.4424" width="4" height="4" fill="#82FF7A"/>
+          <rect data-accent x="90.9617" y="84.4424" width="4" height="4" fill="#82FF7A"/>
+          <rect data-accent x="90.9617" y="186.442" width="4" height="4" fill="#82FF7A"/>
+          <rect data-accent x="207.962" y="84.4424" width="4" height="4" fill="#82FF7A"/>
+          <rect data-accent x="209.212" y="186.442" width="4" height="4" fill="#82FF7A"/>
+         </g>
+     </g>
+
+     <g data-draw-group>
+        <g data-cube-group="2">
+         <path data-stroke-path d="M289.924 129.705L289.672 129.849L240.641 157.915L240.393 158.057L240.145 157.916L190.772 129.85L190.519 129.706V44.2441L190.769 44.0996L190.81 44.0762L240.143 15.6436L240.393 15.5L240.643 15.6445L289.675 44.1006L289.924 44.2451V129.705Z" fill="" stroke="#136A0D"/>
+         <path data-stroke-path d="M240.134 70.5303V171.506L181.596 138.172V36.7324L240.134 70.5303ZM299.259 138.173L241.134 171.503V70.5283L299.259 36.792V138.173ZM298.764 35.9238L240.633 69.6641L182.146 35.8955L240.632 2.12695L298.764 35.9238Z" stroke="#82FF7A"/>
+         <circle data-accent cx="240.635" cy="69.8277" r="1.65385" fill="#82FF7A"/>
+         <circle data-accent cx="240.635" cy="171.539" r="1.65385" fill="#82FF7A"/>
+         <rect data-accent x="238.442" width="4" height="4" fill="#82FF7A"/>
+         <rect data-accent x="179.442" y="34" width="4" height="4" fill="#82FF7A"/>
+         <rect data-accent x="179.442" y="136" width="4" height="4" fill="#82FF7A"/>
+         <rect data-accent x="296.442" y="34" width="4" height="4" fill="#82FF7A"/>
+         <rect data-accent x="297.692" y="136" width="4" height="4" fill="#82FF7A"/>
+        </g>
+     </g>
     </svg>
 
   )
